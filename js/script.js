3a2e06c03835415f67b7c484526942df8d7e59bf
Chris Whong <chris.whong@socrata.com>
2015-02-26 15:05:37 -0500

    Formatting front page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,44 @@
-// fade out splash screen after 1.8 seconds
-setTimeout(function(){
-  $('.splashScreen').fadeOut();
-},1800)
+$( document ).ready(function() {
+
+  // fade out splash screen after 1.8 seconds
+  setTimeout(function(){
+    $('.splashScreen').fadeOut();
+  },1800)
+
+  //listen for enter on number lookup 
+  $('#stopInput').keypress(function(e) {
+      if(e.which == 13) {
+          var reqStopNumber = $(this).val();
+          console.log(reqStopNumber);
+          getStopData(reqStopNumber);
+      }
+  });
+       
+
+  function getStopData(reqStopNumber) {
+       
+
+    $.getJSON('http://mtabustrack.herokuapp.com/stoptimes/' + reqStopNumber, function(data){
+
+      console.log(data);
+
+      if (data.stop_times.length>0) {
+        
+      }
+
+      // $('#stopId').text(stop_id);
+      // $('#stopName').text(data.stop_name);
+
+      // data.stop_times.forEach(function(s){
+      //   var appendString = '<p>' + s.route_number + ' - ' + s.route_name + " " + s.direction_name + " - " + moment(s.stop_time).format('h:mm a') + " ";
+
+      //   appendString += s.realtime ? '(estimated)' : '(scheduled)';
+
+      //   $('#arrivalList').append(appendString);
+      //   $('.results').show();
+      // });
+    });
+  }
+      
+
+});
